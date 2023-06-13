4a24734cffa94526789aee94298fafbb8d9e1f19
lyubimaM <118346040+lyubimaM@users.noreply.github.com>
2023-06-13 22:27:52 +0300

    Last changes

--- src/main/java/com/project/hospital/service/PatientService.java
+++ src/main/java/com/project/hospital/service/PatientService.java
@@ -13,6 +13,9 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import javax.mail.internet.MimeMessage;
 
 import java.time.LocalDateTime;
 import java.util.List;
